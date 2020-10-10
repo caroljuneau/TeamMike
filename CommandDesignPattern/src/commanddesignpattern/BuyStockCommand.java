1840d2cc27f0fd331432728241bfecc0685421a1
Chase Henry <chasemh@email.sc.edu>
2020-10-10 18:43:28 -0400

    Committed changes to BuyStockCommand

--- CommandDesignPattern/src/commanddesignpattern/BuyStockCommand.java
+++ CommandDesignPattern/src/commanddesignpattern/BuyStockCommand.java
@@ -1,16 +1,26 @@
 package commanddesignpattern;
 
 public class BuyStockCommand implements Command {
+
+	/**
+	* Private Variables
+	*/
 	private StockTrade stockTrade;
 	private String stockName;
 	private int shares;
 	
+	/**
+	* Defines Variables
+	*/
 	public BuyStockCommand(StockTrade stockTrade, String stockName, int shares) {
 		this.stockTrade = stockTrade;
 		this.stockName = stockName;
 		this.shares = shares;
 	}
 	
+	/**
+	* Executes stockName with shares
+	*/
 	public void execute() {
 		stockTrade.buy(stockName, shares);
 	}
--- CommandDesignPattern/src/commanddesignpattern/SellStockCommand.java
+++ CommandDesignPattern/src/commanddesignpattern/SellStockCommand.java
@@ -1,16 +1,26 @@
 package commanddesignpattern;
 
 public class SellStockCommand implements Command {
+
+	/**
+	* Variables
+	*/
 	private StockTrade stockTrade;
 	private String stockName;
 	private int shares;
 	
+	/**
+	* Defines Variables
+	*/
 	public SellStockCommand(StockTrade stockTrade, String stockName, int shares) {
 		this.stockTrade = stockTrade;
 		this.stockName = stockName;
 		this.shares = shares;
 	}
 	
+	/**
+	* Executes stockName with shares
+	*/
 	public void execute() {
 		stockTrade.sell(stockName, shares);
 	}
