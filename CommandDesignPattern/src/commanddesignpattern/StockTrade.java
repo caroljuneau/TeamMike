f1e765834c7d123d266767de63b8c761dbf8be12
Collinframes <70244556+Collinframes@users.noreply.github.com>
2020-10-10 17:01:16 -0400

    this is my commit for StockDriver

---
+++ CommandDesignPattern/src/commanddesignpattern/StockTrade.java
@@ -0,0 +1,18 @@
+package commanddesignpattern;
+
+public class StockTrade {
+	
+	public StockTrade() {
+		
+	}
+	
+	public void buy(String stockName, int numShares) {
+		System.out.println("Buying " + numShares + " of " + stockName);
+	}
+	
+	public void sell(String stockName, int numShares) {
+		System.out.println("Selling " + numShares + " of " + stockName);
+	}
+	
+
+}
