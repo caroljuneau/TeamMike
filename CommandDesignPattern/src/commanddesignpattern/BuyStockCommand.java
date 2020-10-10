7cf20b53ada3aec9d60fdb2e2f56c5a9d8e91d40
caroljuneau <cjuneau@email.sc.edu>
2020-10-10 18:22:06 -0400

    added BuyStockCommand

---
+++ CommandDesignPattern/src/commanddesignpattern/BuyStockCommand.java
@@ -0,0 +1,18 @@
+package commanddesignpattern;
+
+public class BuyStockCommand implements Command {
+	private StockTrade stockTrade;
+	private String stockName;
+	private int shares;
+	
+	public BuyStockCommand(StockTrade stockTrade, String stockName, int shares) {
+		this.stockTrade = stockTrade;
+		this.stockName = stockName;
+		this.shares = shares;
+	}
+	
+	public void execute() {
+		stockTrade.buy(stockName, shares);
+	}
+
+}
