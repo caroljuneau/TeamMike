567c621a059a5a86a90dc4de58a5dc0cb5a5b38b
Christopher Walls <clwalls@email.sc.edu>
2020-10-22 12:10:55 -0400

    added data constants for propertymanager, student, and property

---
+++ HousingSystem/src/DataConstants.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+public abstract class DataConstants {	
+	/** Filepaths to JSON Data */
+	protected static final String MANAGER_FILE_NAME = "../JSONExamples/PropertyManager.JSON";
+	protected static final String STUDENT_FILE_NAME = "../JSONExamples/students.json";
+	protected static final String LEASE_FILE_NAME = "../JSONExamples/leases.json";
+	protected static final String PROPERTY_FILE_NAME = "../JSONExamples/properties.JSON";
+	protected static final String REVIEW_FILE_NAME = "../JSONExamples/reviews.json";
+	
+	/** Shared Account Data for PropertyManager and Student */
+	protected static final String ID = "id";
+	protected static final String USER_NAME = "username";
+	protected static final String FIRST_NAME = "firstName";
+	protected static final String LAST_NAME = "lastName";
+	protected static final String EMAIL = "emailAddress";
+	protected static final String PHONE = "phone";
+	/** @todo
+	 * replace ratings, reviews, and leases by corresponding IDS
+	 * protected static final String RATINGS = "ratings";
+	 * protected static final String REVIEWS = "reviews";
+	 * protected static final String LEASES = "signedLeases";
+	 */
+	
+	/** Strings to read Property Manager JSON File */
+	protected static final String MANAGER = "propertyManager";
+	protected static final String MANAGER_PROPERTY = "propertyIds";
+
+	/** Strings to read Students JSON File */
+	protected static final String STUDENT = "studentId";
+	
+	/** Strings to read Properties JSON File */
+	protected static final String PROPERTY = "properties";
+	protected static final String PROPERTY_AMENITIES = "amenities";
+	protected static final String PROPERTY_UTILITIES = "utilities";
+	protected static final String PROPERTY_LOCATION = "location";
+	protected static final String PROPERTY_PICTURES = "pictures";
+	protected static final String PROPERTY_PRICE = "price";
+	protected static final String PROPERTY_REVIEWS = "reviews";
+	protected static final String PROPERTY_BEDS = "beds";
+	protected static final String PROPERTY_BATHS = "baths";
+	/** 
+	 * protected static final String PROPERTY_LEASE = "properties";
+	 * @todo TO BE REPLACED BY leaseID
+	 */
+	protected static final String PROPERTY_DESCRIPTION = "description";
+	protected static final String PROPERTY_CONTACT = "contact";
+	protected static final String PROPERTY_VISIBLE = "visible";
+
+
+
+
+
+
+
+
+
+
+	
+	
+	
+}
