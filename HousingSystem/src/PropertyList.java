fcdc9106fad23ddc1281f11a078e1d249e2100fe
Collinframes <70244556+Collinframes@users.noreply.github.com>
2020-10-27 13:39:52 -0400

    propertList singleton maybe but prob not bc it sucks

--- HousingSystem/src/PropertyList.java
+++ HousingSystem/src/PropertyList.java
@@ -3,11 +3,28 @@ import java.util.ArrayList;
 public class PropertyList {
 	//TODO make iterable
 	//TODO make singleton
-	private ArrayList<PropertyList> PropertyList() {
-		return null;
-	}
-	
+	private static PropertyList propertyList;
+	private ArrayList<Property> properties;
+		
 	private void propertyName(String name) {
 		
+	}
+	
+	private PropertyList() {
+		properties = new ArrayList<Property>();
+	}
+	
+	public static PropertyList getInstance()
+	{
+		if(propertyList == null)
+		{
+			propertyList = new PropertyList();
+		}
+		return propertyList;
+	}
+	
+	public String toString()
+	{
+		return propertyList.toString();
 	}
 }
