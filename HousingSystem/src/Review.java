da6de5cf2ce0bd560ab30f5f793fd53f7404a584
Chase Henry <chasemh@email.sc.edu>
2020-10-28 20:56:23 -0400

    Update Review.java
    changes to Review.java

--- HousingSystem/src/Review.java
+++ HousingSystem/src/Review.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class Review {
 
 	private int rating;
+	private int ratingID;
 	private String username;
 	private String description;
 
@@ -24,4 +25,32 @@ public class Review {
 		s = super.toString();
 		return s;
 	}
+
+	public int getRatingID() {
+    return ratingID;
+  }
+
+  public void setRatingID(int ratingID) {
+    this.ratingID = ratingID;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public void setRating(int rating) {
+    this.rating = rating;
+  }
 }
