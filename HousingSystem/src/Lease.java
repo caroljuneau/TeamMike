1de0a3ecba3fd3ac9d96866f43b977c69752c10b
Christopher Walls <45440801+clwalls@users.noreply.github.com>
2020-10-28 20:44:32 -0400

    finished lease

--- HousingSystem/src/Lease.java
+++ HousingSystem/src/Lease.java
@@ -10,16 +10,65 @@ public class Lease {
 	private boolean signed;
 	private ArrayList<String> signedBy;
 
-	public Lease(Int propertyID, String fees, String repairs, String termination, String info) {
-		this.property = property;
+	public Lease(int propertyID, String fees, String repairs, String termination, String info) {
+		this.propertyID = propertyID;
 		this.fees = fees;
 		this.repairs = repairs;
 		this.termination = termination;
 		this.info = info;
+		signedBy = null;
+	}
+	public int getPropertyID() {
+		return propertyID;
 	}
 
+	public void setPropertyID(int propertyID) {
+		this.propertyID = propertyID;
+	}
+
+	public String getFees() {
+		return fees;
+	}
+
+	public void setFees(String fees) {
+		this.fees = fees;
+	}
+
+	public String getRepairs() {
+		return repairs;
+	}
+
+	public void setRepairs(String repairs) {
+		this.repairs = repairs;
+	}
+
+	public String getTermination() {
+		return termination;
+	}
+
+	public void setTermination(String termination) {
+		this.termination = termination;
+	}
+
+	public String getInfo() {
+		return info;
+	}
+
+	public void setInfo(String info) {
+		this.info = info;
+	}
+
+	public void setSigned(boolean signed) {
+		this.signed = signed;
+	}
+
+	public void setSignedBy(ArrayList<String> signedBy) {
+		this.signedBy = signedBy;
+	}
+	
+
 	public void sign(String fullName) {
-		this.fullname = fullname;
+		signedBy.add(fullName);
 	}
 
 	public boolean getSigned() {
@@ -29,10 +78,19 @@ public class Lease {
 	public ArrayList<String> getSignedBy() {
 		return signedBy;
 	}
-
+	public String getSignedByString() {
+		String s = "";
+		for(String i: signedBy) {
+			s += i + ", ";
+		}
+		return s;
+	}
 	public String toString() {
 		String s;
-		s = super.toString();
+		s = "Property ID: " + this.propertyID + "\nFees: " + this.fees + 
+				"\nRepairs: " + this.repairs + "\nTermination: " + this.termination +
+				"\nInfo: " + this.info;
+		s += "\nSigned By: " + getSignedByString();
 		return s;
 	}
 }
