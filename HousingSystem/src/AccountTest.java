230dd67a97a52f774e0df715a58f24f99f130301
Chase Henry <chasemh@email.sc.edu>
2020-11-16 05:58:35 -0500

    Update AccountTest.java
    Commented out bad code that is just being used as a template. Not sure if this class is actually needed?

--- HousingSystem/src/AccountTest.java
+++ HousingSystem/src/AccountTest.java
@@ -9,33 +9,38 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class AccountTest {
-	private ReviewList review = ReviewList.getInstance();
-	private ArrayList<Account> reviewList = review.getReviews();
-	
-	@BeforeEach
-	public void setup() {
-		reviewList.clear();
-		reviewList.add(new Account(1, "", "", "", "Chase Henry", "Description", ""));
-		reviewList.add(new Account(1, "", "", "", "Chase Henry", "Description", ""));
-		DataWriter.saveReview();
-	}
-	
-	@AfterEach
-	public void tearDown() {
-		ReviewList.getInstance().getReviews().clear();
-		DataWriter.saveReview();
-	}
-	
-	
-	@Test
-	void testHaveValidFirstReviewUser() {
-		boolean idcheck1 = review.haveUser("Chase Henry");
-		assertTrue(idcheck1);
-	}
-	
-	@Test
-	void testHaveValidSecondReviewUser() {
-		boolean idcheck2 = review.haveUser("Chazz Henry");
-		assertTrue(idcheck2);
-	}
+	/**
+	 * WIP
+	 * Not sure if this class is actually even needed since testing is broken down into the more specific
+	 * parts of the account.
+	 */
+//	private Account account = Account.getInstance();
+//	private ArrayList<Account> Account = account.getReviews();
+//	
+//	@BeforeEach
+//	public void setup() {
+//		Account.clear();
+//		Account.add(new Account(1, "", "", "", "Chase Henry", "Description", ""));
+//		Account.add(new Account(1, "", "", "", "Chase Henry", "Description", ""));
+//		DataWriter.saveReview();
+//	}
+//	
+//	@AfterEach
+//	public void tearDown() {
+//		ReviewList.getInstance().getReviews().clear();
+//		DataWriter.saveReview();
+//	}
+//	
+//	
+//	@Test
+//	void testHaveValidFirstReviewUser() {
+//		boolean idcheck1 = account.haveUser("Chase Henry");
+//		assertTrue(idcheck1);
+//	}
+//	
+//	@Test
+//	void testHaveValidSecondReviewUser() {
+//		boolean idcheck2 = account.haveUser("Chazz Henry");
+//		assertTrue(idcheck2);
+//	}
 }
