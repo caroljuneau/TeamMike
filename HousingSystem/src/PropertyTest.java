8142cfd3601b4decd2153394cf2d20330823609d
Chase Henry <chasemh@email.sc.edu>
2020-11-15 20:02:25 -0500

    Update PropertyTest.java
    Fixed error

--- HousingSystem/src/PropertyTest.java
+++ HousingSystem/src/PropertyTest.java
@@ -9,14 +9,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class PropertyTest {
+	private static final boolean[] TRUE = null;
+	private static final boolean[] FALSE = null;
 	private PropertyList property = PropertyList.getInstance();
 	private ArrayList<Property> propertyList = property.getProperties();
 	
 	@BeforeEach
 	public void setup() {
 		propertyList.clear();
-		propertyList.add(new Property(1, TRUE, "pool", "Bluff Rd", 1, 1, 1, "Description", "Contact", TRUE));
-		propertyList.add(new Property(2, FALSE, "pool", "Olympia Rd", 2, 2, 2, "Description", "Contact", FALSE));
+		propertyList.add(new Property(1, TRUE, "pool", "Bluff Rd", 1, 1, 1, "Description", "Contact", true));
+		propertyList.add(new Property(2, FALSE, "pool", "Olympia Rd", 2, 2, 2, "Description", "Contact", false));
 		DataWriter.saveReview();
 	}
 	
